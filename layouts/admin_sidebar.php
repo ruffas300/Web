792f28924f4f1c66f279cb539a0b94637802cafb
ruffas300 <ajrussoiii@gmail.com>
2018-03-22 15:07:16 -0400

    remove admin features

--- layouts/admin_sidebar.php
+++ layouts/admin_sidebar.php
@@ -9,8 +9,6 @@
             <li <?php echo admin_sidebar_active("businessunit_management.php", "add_businessunit.php", "edit_businessunit.php"); ?>><a href="businessunit_management.php">Business Units</a></li>
             <li <?php echo admin_sidebar_active("department_management.php", "add_department.php", "edit_department.php"); ?>><a href="department_management.php">Departments</a></li>
             <li <?php echo admin_sidebar_active("category_management.php", "add_category.php", "edit_category.php"); ?>><a href="category_management.php">Recognition Categories</a></li>
-            <li <?php echo admin_sidebar_active("reward_management.php", "add_reward.php", "edit_reward.php"); ?>><a href="reward_management.php">Rewards</a></li>
-            <li <?php echo admin_sidebar_active("service_management.php", "award_service.php", "add_service.php", "config_service.php"); ?>><a href="service_management.php">Service Awards</a></li>
             <li <?php echo admin_sidebar_active("role_management.php", "add_role.php", "edit_role.php"); ?>><a href="role_management.php">Roles</a></li>
             <li <?php echo admin_sidebar_active("approval_management.php"); ?>><a href="approval_management.php">Approvals</a></li>
             <li <?php echo admin_sidebar_active("report_management.php"); ?>><a href="report_management.php">Reports</a></li>
